00e1bb7b4b8ac2e18e1551148974c802fb3412d5
Oksana Jarosch <ok.jarosch@gmail.com>
2024-07-11 11:15:16 +0200

    starter files and settings

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function HomeScreen() {
+    return (
+        <View className="flex-1 justify-center items-center">
+            <Text>Hallo!</Text>
+        </View>
+    );
+}
