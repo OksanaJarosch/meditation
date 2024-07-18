ef1295188053f8e52d4092443aed714ca21eb6d8
Oksana Jarosch <ok.jarosch@gmail.com>
2024-07-18 11:32:46 +0200

    audio for meditation

--- app/meditate/[id].tsx
+++ app/meditate/[id].tsx
@@ -5,6 +5,8 @@ import { AntDesign } from '@expo/vector-icons';
 import { router, useLocalSearchParams } from 'expo-router';
 import MEDITATION_IMAGES from '@/constants/meditation-images';
 import CustomBtn from '@/components/CustomBtn';
+import { Audio } from 'expo-av';
+import { MEDITATION_DATA, AUDIO_FILES} from '@/constants/MeditationData';
 
 const Meditate = () => {
     const {id} = useLocalSearchParams();
@@ -12,6 +14,8 @@ const Meditate = () => {
 
     const [remainingSec, setRemainingSec] = useState(meditatingTime);
     const [isMeditating, setIsMeditating] = useState(false);
+    const [audioSound, setAudioSound] = useState<Audio.Sound>();
+    const [isPlayingAudio, setIsPlayingAudio] = useState(false);
 
     useEffect(() => {
         let timerId: NodeJS.Timeout;
@@ -19,6 +23,7 @@ const Meditate = () => {
         if (remainingSec === 0) {
             setIsMeditating(false);
             setRemainingSec(meditatingTime);
+            toggleSound();
             return;
         }
 
@@ -33,9 +38,48 @@ const Meditate = () => {
         }
     }, [remainingSec, isMeditating])
 
+    // Turn off audio when closing
+    useEffect(() => {
+        return () => {
+            audioSound?.unloadAsync();
+        }
+    }, [audioSound])
+    
+
     // Time format
     const formattedMinutes = String(Math.floor(remainingSec/60)).padStart(2, "0");
     const formattedSeconds = String(remainingSec%60).padStart(2, "0");
+
+    // audio
+    const initializeSound = async () => {
+        const audioName = MEDITATION_DATA[Number(id) - 1].audio;
+
+        const {sound} = await Audio.Sound.createAsync(AUDIO_FILES[audioName]);
+        setAudioSound(sound);
+        return sound;
+    }
+
+    const toggleSound = async () => {
+        const sound = audioSound ? audioSound : await initializeSound();
+        const status = await sound?.getStatusAsync();
+
+        if (status?.isLoaded && !isPlayingAudio) {
+            await sound.playAsync();
+            setIsPlayingAudio(true);
+        } else {
+            await sound.pauseAsync();
+            setIsPlayingAudio(false);
+        }
+    }
+
+    const startMeditation = async () => {
+        if (remainingSec === meditatingTime) {
+            setIsMeditating(true);
+            setRemainingSec(meditatingTime);
+            await toggleSound();
+            return;
+        }
+    }
 
 
     return (
@@ -48,15 +92,14 @@ const Meditate = () => {
 
                     <View className='flex-1 justify-center'>
                         <View className='mx-auto bg-neutral-200 rounded-full w-44 h-44 justify-center items-center'>
-                            <Text className='text-4xl text-blue-800'>
+                            <Text className='text-4xl text-blue-800 font-rmono'>
                                 {formattedMinutes}:{formattedSeconds}
                             </Text>
                         </View>
                     </View>
 
                     <View className='mb-5'>
-                        <CustomBtn title="Start Meditation" onPress={() => setIsMeditating(true)
-                        }></CustomBtn>
+                        <CustomBtn title="Start Meditation" onPress={startMeditation} disabled={isMeditating} containerStyles={isMeditating ? 'bg-gray-400' : ''}></CustomBtn>
                     </View>
                 </AppGradient>
             </ImageBackground>
--- components/CustomBtn.tsx
+++ components/CustomBtn.tsx
@@ -5,16 +5,18 @@ interface CustomBtnProps {
     onPress: () => void;
     title: string;
     textStyles?: string;
+    disabled?: boolean;
     containerStyles?: string;
 }
 
 
-const CustomBtn = ({onPress, title, textStyles = "", containerStyles = ""}: CustomBtnProps) => {
+const CustomBtn = ({onPress, title, textStyles = "", disabled = false, containerStyles = ""}: CustomBtnProps) => {
     return (
         <TouchableOpacity
             activeOpacity={0.7}
             className={`bg-white rounded-xl min-h-[62px] justify-center items-center ${containerStyles}`}
             onPress={onPress}
+            disabled={disabled}
         >
         <Text className={`font-semibold text-lg ${textStyles}`}>
             {title}
